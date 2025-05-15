55f848750094f7883ccafce1740a7fb23054296c
kaushalshah750 <kaushalshah750@gmail.com>
2025-05-15 17:09:04 +0530

    Added SEO

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   standalone: true
 })
 export class NavbarComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,17 @@ module.exports = {
     ],
     theme: {
         extend: {
-        colors: {
-            primary: '#0A2647',
-            secondary: '#2C74B3',
-            accent: '#F6C90E',
-            light: '#F3F6FA',
-            darkText: '#1C1C1C',
-            mutedText: '#6B7280',
-        },
+            transitionProperty: {
+                height: 'height',
+            },
+            colors: {
+                primary: '#0A2647',
+                secondary: '#2C74B3',
+                accent: '#F6C90E',
+                light: '#F3F6FA',
+                darkText: '#1C1C1C',
+                mutedText: '#6B7280',
+            },
         },
     },
     plugins: [],
